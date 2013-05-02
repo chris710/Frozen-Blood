76cf5d8f37a6e00a9d07a7ef96d1d49a51a89b3f
Krzesimir Samborski <chris710@o2.pl>
2013-05-02 11:41:57 +0200

    (PRE-ALFA) EDYTOR POZIOMÓW
    W sumie niczego jeszcze nie napisałem poza stworzeniem TO-DO LIST

---
+++ Kod/editor/level_editor.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////
+///                                 ///
+///                                 ///
+///         EDYTOR POZIOMÓW         ///
+///                                 ///
+///                                 ///
+///////////////////////////////////////
+
+
+
+
+//      TO-DO LIST:
+//  1)Tworzenie mapy jako macierzy 2wymiarowej
+//  2)Stworzenie róznych rodzajów terenów (teren otwarty, woda, most, las, góry, osada, twierdza, droga) lub...
+//  3)Pobranie zasobów (głównie obrazków i właściwości terenu) ze źródeł zewnętrznych
+//  4)Stworzenie narzędzia do "ręcznego" zapełniania mapy (GUI?)
+//  5)Użycie presetów terenu do wygenerowania mapy
