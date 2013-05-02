e16a4965846942ef078f963334212eabdbcfe7c4
Krzesimir Samborski <chris710@o2.pl>
2013-05-02 15:36:06 +0200

    (BETA) AKTUALIZACJA README/ KLASY JEDNOSTEK
    Dodane zasady gry.

--- Kod/game/units.cpp
+++ Kod/game/units.cpp
@@ -1,4 +1,17 @@
 
-
+//////////////////////////////////////////
+//
+//      GŁÓWNA KLASA JEDNOSTEK
+//
+/////////////////////////////////////////
+#include "game.h"
 
 class unit
+{
+    public:
+        int health;     //punkty zdrowia, domyślnie 100
+        int Xpos,Ypos;      //położenie X-owe i Y-owe na mapie
+        int attack;     //poziom ataku
+        int defence;    //poziom obrony
+        string type;    //rodzaj jednostki
+};
